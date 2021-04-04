baf416371ad7a10e4deb485505a68c2f025afb66
erasromani <ekr6072@nyu.edu>
2021-04-04 13:00:54 -0400

    test

--- grasping_scenarios/grasp_object.py
+++ grasping_scenarios/grasp_object.py
@@ -19,7 +19,7 @@ from omni.isaac.utils._isaac_utils import math as math_utils
 from omni.isaac.samples.scripts.utils.world import World
 from omni.isaac.samples.scripts.utils.franka import Franka, default_config
 
-from .scenario import set_translate, set_rotate, create_franka, Scenario, setup_physics
+from .scenario import set_translate, set_rotate, create_solid_franka, Scenario, setup_physics
 from copy import copy
 
 from omni.physx import _physx
