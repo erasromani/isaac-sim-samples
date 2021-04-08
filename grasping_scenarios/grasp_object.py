9f7cd4dc82c00b76620c85930a97584f5784537e
erasromani <ekr6072@nyu.edu>
2021-04-07 23:22:10 -0400

    updated get_target_to_object method

--- grasping_scenarios/grasp_object.py
+++ grasping_scenarios/grasp_object.py
@@ -260,14 +260,15 @@ class PickAndPlaceStateMachine(object):
         offset = _dynamic_control.Transform()
         # offset.p.x = -offset_up
 
-        offset.p.z = 10
+        # offset.p.z = -10
         # offset.r = (0, 0, 0, 1)
         body_handle = self.dc.get_rigid_body(self.current)
         obj_pose = self.dc.get_rigid_body_pose(body_handle)
         target_position = _dynamic_control.Transform()
         target_position.p = obj_pose.p
+        target_position.p.z = 1000
         target_position.r = [0.0, 1.0, 0.0, 0.0]
-        target_position = math_utils.mul(target_position, offset)
+        # target_position = math_utils.mul(target_position, offset)
         target_position.p = math_utils.mul(target_position.p, 0.01)
         return target_position
 
