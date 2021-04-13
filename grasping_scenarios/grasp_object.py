ef474ec587190fcb4cf0a24dc0952e05df366918
erasromani <ekr6072@nyu.edu>
2021-04-13 17:44:27 -0500

    added offset_position

--- grasping_scenarios/grasp_object.py
+++ grasping_scenarios/grasp_object.py
@@ -423,7 +423,7 @@ class PickAndPlaceStateMachine(object):
         ensures the bin obstacle is suppressed for the planner, Updates the target position
         to where the bin is, and send the robot to move towards it. No change of state happens
         """
-        self.set_target_to_object(offset_position=[0, 0, 10], n_waypoints=1, True)
+        self.set_target_to_object(offset_position=[0, 0, 10], n_waypoints=1, clear_waypoints=True)
         self.move_to_target()
 
     # def _holding_goal_reached(self, *args):
