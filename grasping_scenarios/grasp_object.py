2cca9ce21c348c6cb540b7663bb8017bd175c889
erasromani <ekr6072@nyu.edu>
2021-04-20 00:57:24 -0400

    added lifting into sequence

--- grasping_scenarios/grasp_object.py
+++ grasping_scenarios/grasp_object.py
@@ -301,15 +301,15 @@ class PickAndPlaceStateMachine(object):
 
         # NOTE: This may be a good way to evaluate whether the graps was a success or failure
         finger_velocity = self.robot.end_effector.gripper.get_velocity(from_articulation=True)
-        # carb.log_warn(f'WIDTH: {self.robot.end_effector.gripper.width:.4f}, ACTUAL WIDTH: {self.robot.end_effector.gripper.get_width():.4f}, FINGER_VELOCITY: ({finger_velocity[0]:.4f}, {finger_velocity[1]:.4f}), HISTORY_STD: {np.array(self.robot.end_effector.gripper.width_history).std():.2e}')
+        carb.log_warn(f'WIDTH: {self.robot.end_effector.gripper.width:.4f}, ACTUAL WIDTH: {self.robot.end_effector.gripper.get_width():.4f}, FINGER_VELOCITY: ({finger_velocity[0]:.4f}, {finger_velocity[1]:.4f}), HISTORY_STD: {np.array(self.robot.end_effector.gripper.width_history).std():.2e}')
         # if self.is_closed and (self.current_state == SM_states.GRASPING or self.current_state == SM_states.LIFTING):
         if self.current_state == SM_states.GRASPING or self.current_state == SM_states.LIFTING:
             # object grasped
-            if not self.robot.end_effector.gripper.is_closed(1e-2) and not self.robot.end_effector.gripper.is_moving(1e-2):
+            if not self.robot.end_effector.gripper.is_closed(1e-1) and not self.robot.end_effector.gripper.is_moving(1e-2):
                 self._attached = True
                 # self.is_closed = False
             # object not grasped
-            elif self.robot.end_effector.gripper.is_closed(1e-2):
+            elif self.robot.end_effector.gripper.is_closed(1e-1):
                 self._detached = True
                 self.is_closed = True
 
